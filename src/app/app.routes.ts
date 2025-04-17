930d6a196003db830b6ccf1bf049c1c3b401c892
JFenderson <fenderson.joseph@gmail.com>
2025-04-17 01:35:56 -0500

    netlify fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { ChapterListComponent } from './chapter-list/chapter-list.component';
+import { MemberDirectoryComponent } from './member-directory/member-directory.component';
 
 export const routes: Routes = [];
