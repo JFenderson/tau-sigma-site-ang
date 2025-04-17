3338ac592627245743144f18cdd701c4b3d8ee33
JFenderson <fenderson.joseph@gmail.com>
2025-04-17 00:56:29 -0500

    initial commit

---
+++ src/app/member-directory/member-directory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-directory',
+  imports: [],
+  templateUrl: './member-directory.component.html',
+  styleUrl: './member-directory.component.css'
+})
+export class MemberDirectoryComponent {
+
+}
